fcd6152854c3162b52871b9c4ef17057aa29b924
eric <ericlam@vitessedata.com>
2023-04-03 15:49:23 +0100

    remove KitePagesStore

--- src/main/java/com/facebook/presto/plugin/kite/KitePageSinkProvider.java
+++ src/main/java/com/facebook/presto/plugin/kite/KitePageSinkProvider.java
@@ -22,17 +22,20 @@ import com.facebook.presto.spi.ConnectorSession;
 import com.facebook.presto.spi.HostAddress;
 import com.facebook.presto.spi.NodeManager;
 import com.facebook.presto.spi.PageSinkContext;
+import com.facebook.presto.spi.PrestoException;
 import com.facebook.presto.spi.connector.ConnectorPageSinkProvider;
 import com.facebook.presto.spi.connector.ConnectorTransactionHandle;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableList;
 import io.airlift.slice.Slice;
+import io.airlift.slice.Slices;
 
 import javax.inject.Inject;
 
 import java.util.Collection;
 import java.util.concurrent.CompletableFuture;
 
+import static com.facebook.presto.spi.StandardErrorCode.NOT_SUPPORTED;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkState;
 import static java.util.Objects.requireNonNull;
@@ -43,19 +46,17 @@ public class KitePageSinkProvider
 {
     private static final Logger log = Logger.get(KitePageSinkProvider.class);
 
-    private final KitePagesStore pagesStore;
     private final HostAddress currentHostAddress;
 
     @Inject
-    public KitePageSinkProvider(KitePagesStore pagesStore, NodeManager nodeManager)
+    public KitePageSinkProvider(NodeManager nodeManager)
     {
-        this(pagesStore, requireNonNull(nodeManager, "nodeManager is null").getCurrentNode().getHostAndPort());
+        this(requireNonNull(nodeManager, "nodeManager is null").getCurrentNode().getHostAndPort());
     }
 
     @VisibleForTesting
-    public KitePageSinkProvider(KitePagesStore pagesStore, HostAddress currentHostAddress)
+    public KitePageSinkProvider(HostAddress currentHostAddress)
     {
-        this.pagesStore = requireNonNull(pagesStore, "pagesStore is null");
         this.currentHostAddress = requireNonNull(currentHostAddress, "currentHostAddress is null");
     }
 
@@ -70,38 +71,23 @@ public class KitePageSinkProvider
         long tableId = tableHandle.getTableId();
         checkState(kiteOutputTableHandle.getActiveTableIds().contains(tableId));
 
-        pagesStore.cleanUp(kiteOutputTableHandle.getActiveTableIds());
-        pagesStore.initialize(tableId);
-        return new KitePageSink(pagesStore, currentHostAddress, tableId);
+        return new KitePageSink(currentHostAddress, tableId);
     }
 
     @Override
     public ConnectorPageSink createPageSink(ConnectorTransactionHandle transactionHandle, ConnectorSession session, ConnectorInsertTableHandle insertTableHandle, PageSinkContext pageSinkContext)
     {
-        log.info("createPageSink insert");
-        checkArgument(!pageSinkContext.isCommitRequired(), "Kite connector does not support page sink commit");
-
-        KiteInsertTableHandle kiteInsertTableHandle = (KiteInsertTableHandle) insertTableHandle;
-        KiteTableHandle tableHandle = kiteInsertTableHandle.getTable();
-        long tableId = tableHandle.getTableId();
-        checkState(kiteInsertTableHandle.getActiveTableIds().contains(tableId));
-
-        pagesStore.cleanUp(kiteInsertTableHandle.getActiveTableIds());
-        pagesStore.initialize(tableId);
-        return new KitePageSink(pagesStore, currentHostAddress, tableId);
+        throw new PrestoException(NOT_SUPPORTED, "Insert not supported");
     }
 
     private static class KitePageSink
             implements ConnectorPageSink
     {
-        private final KitePagesStore pagesStore;
         private final HostAddress currentHostAddress;
         private final long tableId;
-        private long addedRows;
 
-        public KitePageSink(KitePagesStore pagesStore, HostAddress currentHostAddress, long tableId)
+        public KitePageSink(HostAddress currentHostAddress, long tableId)
         {
-            this.pagesStore = requireNonNull(pagesStore, "pagesStore is null");
             this.currentHostAddress = requireNonNull(currentHostAddress, "currentHostAddress is null");
             this.tableId = tableId;
         }
@@ -109,15 +95,13 @@ public class KitePageSinkProvider
         @Override
         public CompletableFuture<?> appendPage(Page page)
         {
-            pagesStore.add(tableId, page);
-            addedRows += page.getPositionCount();
             return NOT_BLOCKED;
         }
 
         @Override
         public CompletableFuture<Collection<Slice>> finish()
         {
-            return completedFuture(ImmutableList.of(new KiteDataFragment(currentHostAddress, addedRows).toSlice()));
+            return completedFuture(ImmutableList.of(Slices.wrappedBuffer(currentHostAddress.toString().getBytes())));
         }
 
         @Override
